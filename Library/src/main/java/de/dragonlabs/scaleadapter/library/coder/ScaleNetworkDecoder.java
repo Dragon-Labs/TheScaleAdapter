62b12e9e20c2684afd51f8f07e62653104660636
Alphyron <admin@dragon-labs.de>
2018-07-11 12:11:12 +0200

    Added the Decoder and Encoder

---
+++ Library/src/main/java/de/dragonlabs/scaleadapter/library/coder/ScaleNetworkDecoder.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 Dragon-Labs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package de.dragonlabs.scaleadapter.library.coder;
+
+import de.dragonlabs.scaleadapter.library.packet.ScalePacket;
+import de.dragonlabs.scaleadapter.library.protocol.PacketManager;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class ScaleNetworkDecoder extends ByteToMessageDecoder
+{
+    private final PacketManager packetManager;
+
+    public ScaleNetworkDecoder(PacketManager packetManager)
+    {
+        this.packetManager = packetManager;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> list) throws Exception
+    {
+        Byte id = byteBuf.readByte();
+
+        ScalePacket packet = packetManager.getPacketById(id);
+
+        if (packet == null) {
+            throw new NullPointerException("Don't find a Packet with the ID " + id);
+        }
+
+        try {
+            packet.setChannel(ctx.channel());
+
+            packet.onRead(new ByteBufInputStream(byteBuf));
+            list.add(packet);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ Library/src/main/java/de/dragonlabs/scaleadapter/library/coder/ScaleNetworkEndcoder.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Dragon-Labs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package de.dragonlabs.scaleadapter.library.coder;
+
+import de.dragonlabs.scaleadapter.library.packet.ScalePacket;
+import de.dragonlabs.scaleadapter.library.protocol.PacketManager;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class ScaleNetworkEndcoder  extends MessageToByteEncoder<ScalePacket>
+{
+    private PacketManager packetManager;
+
+    public ScaleNetworkEndcoder(PacketManager packetManager){
+        this.packetManager = packetManager;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, ScalePacket packet, ByteBuf out) throws Exception {
+
+        Byte id = packetManager.getIdByClass(packet.getClass());
+
+        if(id == null)
+        {
+            throw new NullPointerException("Packet not found (" + packet.getClass() + ")");
+        }
+
+        try {
+            out.writeByte(id);
+            packet.onWrite(new ByteBufOutputStream(out));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
